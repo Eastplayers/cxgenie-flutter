0e884a36adaa5199026ae7fd56f7cffd22a8873a
Quyen Le <quyen.le@eastplayers.io>
2024-01-22 22:07:04 +0700

    update: remove blue dot

--- lib/widgets/ticket_list.dart
+++ lib/widgets/ticket_list.dart
@@ -271,30 +271,31 @@ class TicketListState extends State<TicketList> {
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      if (ticket.isReplied == true)
-                                        Container(
-                                          width: 8,
-                                          height: 8,
-                                          margin: const EdgeInsets.only(top: 4),
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            color: const Color(0xff364DE7),
+                                      // if (ticket.isReplied == true)
+                                      //   Container(
+                                      //     width: 8,
+                                      //     height: 8,
+                                      //     margin: const EdgeInsets.only(top: 4),
+                                      //     decoration: BoxDecoration(
+                                      //       borderRadius:
+                                      //           BorderRadius.circular(10),
+                                      //       color: const Color(0xff364DE7),
+                                      //     ),
+                                      //   ),
+                                      // if (ticket.isReplied == true)
+                                      //   const SizedBox(
+                                      //     width: 8,
+                                      //   ),
+                                      Expanded(
+                                        child: Text(
+                                          ticket.name,
+                                          textAlign: TextAlign.left,
+                                          style: const TextStyle(
+                                            color: Color(0xff2C2E33),
+                                            fontWeight: FontWeight.w700,
                                           ),
                                         ),
-                                      if (ticket.isReplied == true)
-                                        const SizedBox(
-                                          width: 8,
-                                        ),
-                                      Expanded(
-                                          child: Text(
-                                        ticket.name,
-                                        textAlign: TextAlign.left,
-                                        style: const TextStyle(
-                                          color: Color(0xff2C2E33),
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ))
+                                      )
                                     ],
                                   ),
                                   if (ticket.ticketCategory != null)
